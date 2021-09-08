6bcfd8e10251b0ae98ec9779219d2793eb3304c4
lsj8367 <lsj8367@naver.com>
2021-09-09 00:00:54 +0900

    feat: 빌더 패턴 구현

---
+++ src/builder/NewYorkPizza.java
@@ -0,0 +1,32 @@
+package builder;
+
+import java.util.Objects;
+
+public class NewYorkPizza extends Pizza {
+
+    public enum Size { SMALL, MEDIUM, LARGE };
+    private final Size size;
+
+    public static class Builder extends Pizza.Builder {
+        private final Size size;
+
+        public Builder(Size size) {
+            this.size = Objects.requireNonNull(size);
+        }
+
+        @Override
+        public NewYorkPizza build() {
+            return new NewYorkPizza(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+    }
+
+    private NewYorkPizza(Builder builder) {
+        super(builder);
+        size = builder.size;
+    }
+}
