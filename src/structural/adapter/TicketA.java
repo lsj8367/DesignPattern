c24699a758f07e935cd5f73eb20877a2398e004f
SeungJae Lee <lsj8367@naver.com>
2021-09-29 18:01:08 +0900

    remove: 기존 어댑터패턴 삭제

--- src/structural/adapter/TicketA.java
+++
@@ -1,7 +0,0 @@
-package structural.adapter;
-
-public interface TicketA {
-    void choice(int token);
-    void print();
-    void buy();
-}
--- src/structural/adapter/TicketB.java
+++
@@ -1,9 +0,0 @@
-package structural.adapter;
-
-public interface TicketB {
-    void choice(int token);
-    void print();
-    void buyOnOffline();
-    void buyOnOnline();
-    String getMenu();
-}
